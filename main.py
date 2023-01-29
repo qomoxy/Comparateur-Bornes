39637f301486e0184d2d0c55edc281f9a4c85ef7
ilog <darkpowerbow@protonmail.com>
2023-01-29 12:35:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ clock = pygame.time.Clock()
 player = player.Player([pygame.image.load("test1.png"),
                        pygame.image.load("test2.png")])
 
-print("\n\n\nclass events: quitter le jeu, deplacer le joueur, utiliser une attaque etc\nclass joueur: mouvement du joueur (Fleches), vie etc\n SVP separez le code en plusieurs morceaux\n\n\n")
+print("\n\n\nclass events: quitter le jeu, deplacer le joueur, utiliser une attaque etc\nclass joueur: mouvement du joueur (Fleches), vie etc\nclass animations: creer une animation\n SVP separez le code en plusieurs morceaux\n\n\n")
 
 while running:
     events.HandleEvents()
