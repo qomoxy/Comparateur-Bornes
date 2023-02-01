0bef76fde5ae259290393a6b196f311bbf22263c
Qomoxy <66842370+qomoxy@users.noreply.github.com>
2023-02-01 23:15:21 +0100

    menu restore

--- menu/button.py
+++ menu/button.py
@@ -1,33 +1,31 @@
 import pygame
+from pygame.locals import *
+from Base.actions import Action
 
 
 class Button():
-    def __init__(self, x, y, image, scale):
-        width = image.get_width()
-        height = image.get_height()
-        self.image = pygame.transform.scale(
-            image, (int(width * scale), int(height * scale)))
+    def __init__(self, x: int, y: int, imageFolder: str, scale, actionOnClick: Action):
+        self.imageLoad = pygame.image.load(imageFolder)
+        self.width: int = self.imageLoad.get_width()
+        self.height: int = self.imageLoad.get_height()
+        self.scale: int = scale
+        self.image = pygame.transform.scale(self.imageLoad, (int(
+            self.width * self.scale), int(self.height * self.scale)))
+        self.coordonnate: tuple[int, int] = (x, y)
         self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
-        self.clicked = False
+        self.clicked: bool = False
+        self.actionOnClick = actionOnClick
 
-    def draw(self, surface):
-        action = False
-
+    def draw(self, surface) -> None:
         pos = pygame.mouse.get_pos()
 
-        if (
-                self.rect.collidepoint(pos)
-                and pygame.mouse.get_pressed()[0] == 1
-                and self.clicked == False
-        ):
-            self.clicked = True
-            action = True
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                self.actionOnClick.Invoke()
+                print("True")
 
         if pygame.mouse.get_pressed()[0] == 0:
             self.clicked = False
 
-        # draw button on screen
-        surface.blit(self.image, (self.rect.x, self.rect.y))
-
-        return action
+        surface.blit(self.image, (self.coordonnate))
--- menu/menu.py
+++ menu/menu.py
@@ -1,27 +1,36 @@
 import pygame
 from pygame.locals import *
+from button import Button
+from Base.actions import Action
+import menu.menu
+
 pygame.init()
 
 
 class Background(pygame.sprite.Sprite):
     def __init__(self, image_file, location):
-        pygame.sprite.Sprite.__init__(self)  # call Sprite initializer
+        pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(image_file)
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
 
 
-def mainMenu():
-    window = pygame.display.set_mode((0, 0), FULLSCREEN)
-    BackGround = Background('sprites/background_menu.jpeg', [0, 0])
+def mainMenu() -> None:
+    window = pygame.display.set_mode((0, 0), RESIZABLE)
+    BackGround = Background('sprites/menu/background_menu.png', [0, 0])
 
     runing = True
     while runing:
         mousePose = pygame.mouse.get_pos()
 
-        window.fill((0, 0, 0))
         window.blit(pygame.transform.scale(
             BackGround.image, (window.get_width(), window.get_height())), (0, 0))
+
+        startButton = Button(
+            0, 0, 'sprites/menu/startButton.png', 1, Action(menu.menu, 'StartGame'))
+
+        if startButton.draw(window):
+            pass
 
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -33,3 +42,7 @@ def mainMenu():
 
 
 mainMenu()
+
+
+def StartGame():
+    print("StartGame")
